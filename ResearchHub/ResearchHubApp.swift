4f864be6d7fe8d3fa6f8c559e03eb849bdc0b638
KenLee1130 <ken6020255@gmail.com>
2026-06-11 12:15:00 +0800

    Initial Commit

---
+++ ResearchHub/ResearchHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResearchHubApp.swift
+//  ResearchHub
+//
+//  Created by 李紹誠 on 2026/6/11.
+//
+
+import SwiftUI
+
+@main
+struct ResearchHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
